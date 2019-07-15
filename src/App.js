d5a6ff7c04ffeb2cac7d956abeff1333c75b6608
Aveleira <elaaysenur@hotmail.com>
2019-07-15 23:04:48 +0300

    react hooks intro and useState()

--- src/App.js
+++ src/App.js
@@ -1,50 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
-class App extends Component { //'class' has properties, properties are like variables of class
+const app = props => { //'class' has properties, properties are like variables of class
   // in normal JS code    var sth = "some value" => this doesn't work in a class
-  something = "some value" 
 
-  //STATE = one special property you can define in any component which extends Component
-  state = {
-    persons:[
-      {name:"Max", age:28},
-      {name:"Manu", age:29},
-      {name:"Steph", age:26}
-    ],
-    otherStateSth: "some value"
-  } // state's value is a JavaScript object
-  // if state changes it will lead React to re-render our Dom, to update.
-
-  //execute a function of your class, that is called method.
-  //assign a function to a property
-  switchNameHandler = () => {
-    //console.log("was clicked!");
-    //this.state reach to the class App with arrow function otherwise will not reach to the class
-    //DO NOT MUTATE STATE DIRECTLY! this.state.persons[0].name = "Maximilian"
-    this.setState( { persons:[
-        {name:"Maximilian", age:28},
+  const [personsState, setPersonsState] = useState({
+      persons:[
+        {name:"Max", age:28},
         {name:"Manu", age:29},
-        {name:"Steph", age:27}
-      ] 
-    } ) // takes object an as argument and merge whatever we define here with our existing state   
-  }
+        {name:"Steph", age:26}
+      ],
+      otherStateSth: "some value"
+    })
 
-  render() {
+    // can use useState multiple times
+
+    const switchNameHandler = () => {
+        setPersonsState({ 
+          persons:[
+            {name:"Maximilian", age:28},
+            {name:"Manu", age:29},
+            {name:"Steph", age:27}
+          ],
+          otherStateSth:personsState.otherStateSth
+          //with react hooks changing state; does not merge changes with the state it directly changes / replaces old state
+        })   
+      }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button onClick={this.switchNameHandler} >Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} /> 
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler} >Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} /> 
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing </Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className:'App'}, React.createElement('h1',null,'hi, i am react app'))
     //this in Person refers to the class, App
     //onClick={this.switchNameHandler()} if paranthesis, react immediately execute that function.
   }
-}
 
-export default App;
+export default app;
+
+  // //STATE = one special property you can define in any component which extends Component
+  // state = {
+  //   persons:[
+  //     {name:"Max", age:28},
+  //     {name:"Manu", age:29},
+  //     {name:"Steph", age:26}
+  //   ],
+  //   otherStateSth: "some value"
+  // } // state's value is a JavaScript object
+  // // if state changes it will lead React to re-render our Dom, to update.
+
+  // //execute a function of your class, that is called method.
+  // //assign a function to a property
+  // switchNameHandler = () => {
+  //   //console.log("was clicked!");
+  //   //this.state reach to the class App with arrow function otherwise will not reach to the class
+  //   //DO NOT MUTATE STATE DIRECTLY! this.state.persons[0].name = "Maximilian"
+  //   this.setState({ 
+  //     persons:[
+  //       {name:"Maximilian", age:28},
+  //       {name:"Manu", age:29},
+  //       {name:"Steph", age:27}
+  //     ] 
+  //   }) // takes object an as argument and merge whatever we define here with our existing state   
+  // }
