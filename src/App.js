22985699a3ceb00132e3eb96725e800b0c4c67f4
aysenur.ela <aysenur.ela@solvoyo.com>
2019-08-20 09:11:42 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,72 +1,83 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from 'react'
+import './App.css'
+import Person from './Person/Person'
 
-class App extends Component { 
-
-   state = {
-    persons:[
-      {name:"Max", age:28},
-      {name:"Manu", age:29},
-      {name:"Steph", age:26}
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Steph', age: 26 }
     ],
-    otherStateSth: "some value"
-  } 
-
-  switchNameHandler = (newName) => {
-    this.setState({ 
-      persons:[
-        {name:newName, age:28},
-        {name:"Manu", age:29},
-        {name:"Steph", age:27}
-      ] 
-    }) 
+    otherStateSth: 'some value',
+    showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({ 
-      persons:[
-        {name:"Max", age:28},
-        {name:event.target.value , age:29},
-        {name:"Steph", age:26}
-      ] 
-    }) 
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Steph', age: 27 }
+      ]
+    })
   }
 
-  //bind method is more efficient than the arrow syntax
-
-render () {
-
-  const style = {
-    backgroundColor:"white",
-    font: "inherit",
-    border: "1px solid blue",
-    padding: "8px",
-    cursor: "pointer"
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Steph', age: 26 }
+      ]
+    })
   }
+
+  // bind method is more efficient than the arrow syntax
+
+  togglePersonsHandler = () => {
+    const showNames = this.state.showPersons
+    this.setState({ showPersons: !showNames })
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      margin: '10px'
+    }
 
     return (
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <button 
-          style = {style}
-          onClick={() => this.switchNameHandler("Maximilian!!")} >Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} /> 
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Max!")}
-          changed={this.nameChangedHandler}>My hobbies: Racing </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+      <div className='App'>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {this.state.showPersons ? ( // React.createElement() work behind scenes
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Max!')}
+              changed={this.nameChangedHandler}
+            >
+              My hobbies: Racing{' '}
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
       </div>
-    );
+    )
   }
 }
 
-export default App;
-
-
+export default App
